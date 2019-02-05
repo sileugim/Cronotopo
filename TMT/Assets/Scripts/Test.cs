51d38c9b1d0599fd267199976ef2c49e633930b5
Mangel <lmadra.18@gmail.com>
2019-02-04 19:21:28 -0500

    Test 2

--- TMT/Assets/Scripts/Test.cs
+++ TMT/Assets/Scripts/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hola Mundo");
     }
 
     // Update is called once per frame
